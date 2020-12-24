1e0d51d2e9dd5d608c49fa902dd39dcadb2c60b3
Muhammad Ali Mughal <decent_mughal007@hotmail.com>
2020-12-24 21:26:21 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+//alert("Hello World");
+
+// console.log("Hello World");
+
+// document.write("Hello World");
+
+// var a = 5;
+// var b = 10;
+// var c = a + b;
+// alert(c);
+
+
+// var x = 20;
+// var y = "ali";
+// var z = x + y;
+// alert(z);
+
+// var f = 20;
+// var g = "30";
+// var h = f + g;
+// alert(h);
+
+// var a = "30";
+// var b = "40";
+// var c = a / b;
+// alert(c);
+
+
+// var y = 12 % 3;
+// alert(y);
+
+// var h = 9/(4+5)*4;
+// alert(h);
+
+// var a = 5;
+// var b = ++a;
+// alert(b);
+
+// var x = 10;
+// var y = --x;
+// alert(y)
+
+
+// a++
+// a=a+1
+// a=+1
+
+
+// var f = 3;
+// var g = f++ + f - ++f + --f + f - f-- + ++f + --f;
+//         3 + 4 - 5 + 4 + 4 - 4 + 4 + 3;
+// alert(g);
+
+
+// var x = 10;
+// var y = x++ + x + ++x - ++x + --x - x - x-- + x;
+// alert(y);
+
+// var x = prompt("Enter your value","Your value is");
+// var y = 5;
+// var z = x + y;
+// alert(z);
+
+
+var aliMughal
+        
